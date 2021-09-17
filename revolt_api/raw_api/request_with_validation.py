dde3c82a9de585b37945efeea7ab661b3fe89b6c
gresm <>
2021-09-17 18:25:03 +0200

    base_route.py - finished

--- revolt_api/raw_api/request_with_validation.py
+++ revolt_api/raw_api/request_with_validation.py
@@ -11,19 +11,28 @@ __all__ = [
 
 
 class ValidatedRequest:
-    def __init__(self, validation_token: str, is_bot: bool, url: str = "https://api.revolt.chat/", method: str = "GET"):
+    @api_error(cause="Missing token when auth enabled")
+    def __init__(self, is_bot: bool, with_auth: bool = True, validation_token: str = "",
+                 url: str = "https://api.revolt.chat/", method: str = "GET",):
         self.validation_token = validation_token
         self.is_bot = is_bot
         self.base_url = url
         self.method = method
+        self.with_auth = with_auth
         self.request_ref: RequestThread | None = None
+        assert not with_auth or validation_token
 
     @property
     def headers(self):
-        if self.is_bot:
-            return {"x-bot-token": self.validation_token}
-        else:
-            return {"x-session-token": self.validation_token}
+        headers = {}
+
+        if self.with_auth:
+            if self.is_bot:
+                headers["x-bot-token"] = self.validation_token
+            else:
+                headers["x-session-token"] = self.validation_token
+
+        return headers
 
     @headers.setter
     def headers(self, value):
@@ -44,7 +53,7 @@ class ValidatedRequest:
         return dct
 
     def request(self, json: dict | None = None, data: str = "", params: dict[str, str] = None, url: str = "",
-                method: str = "GET", path_args: tuple[str] = (), path_kwargs: dict[str, str] = None,
+                path_args: tuple[str] = (), path_kwargs: dict[str, str] = None,
                 with_ulid: bool = False, *args, **kwargs):
         if path_kwargs is None:
             path_kwargs = {}
@@ -52,10 +61,10 @@ class ValidatedRequest:
         json = self._add_nonce(json, with_ulid)
 
         url = convert_url(url, self.base_url, url_args=path_args, url_kwargs=path_kwargs, *args, **kwargs)
-        return self._run_request_thread(url, method, params, data, json)
+        return self._run_request_thread(url, params, data, json)
 
-    def _run_request_thread(self, url: str, method: str, params: dict[str, str], data: str, json):
-        self.request_ref = RequestThread(url, method, self.headers, params, data, json)
+    def _run_request_thread(self, url: str, params: dict[str, str], data: str, json):
+        self.request_ref = RequestThread(url, self.method, self.headers, params, data, json)
         self.request_ref.start()
         return self.request_ref
 
--- revolt_api/routes/base_route.py
+++ revolt_api/routes/base_route.py
@@ -1,6 +1,30 @@
-from ..raw_api import convert_url
+from __future__ import annotations
+
+from ..raw_api import ValidatedRequest
+from .. import get_url
 
 
 class Route:
-    def __init__(self, url: str):
+    def __init__(self, url: str, method: str, with_auth: bool = True, params: dict[str, str] = None,
+                 with_ulid: bool = False):
         self.url = url
+        self.method = method
+        self.with_auth = with_auth
+        self.params = params
+        self.with_ulid = with_ulid
+
+
+def run_route(route: Route, token: str = None, json: dict | None = None, as_bot: bool = False,
+              url_kwargs: dict[str, str] = None):
+    ret = ValidatedRequest(
+        is_bot=as_bot, with_auth=route.with_auth,
+        validation_token=token, url=get_url(),
+        method=route.method
+    )
+
+    ret.request(
+        json=json, data="",
+        params=route.params, url=route.url,
+        url_kwargs=url_kwargs, with_ulid=route.with_ulid
+    )
+    return ret
