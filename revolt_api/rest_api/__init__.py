c0f42f700b1ca419a01f0382b9f09f87b13c5a89
gresm <>
2021-09-23 08:56:19 +0200

    project tree rearrangement - part 4

--- revolt_api/rest_api/__init__.py
+++ revolt_api/rest_api/__init__.py
@@ -1,7 +1,6 @@
 from .request_handler import *
 from .url_parser import *
 from .request_with_validation import *
-from .routes import *
 
 from . import request_handler
 from . import url_parser
@@ -10,8 +9,8 @@ from . import routes
 
 
 __all__ = [
+    "routes",
     *request_handler.__all__,
     *url_parser.__all__,
     *request_with_validation.__all__,
-    *routes.__all__
 ]
